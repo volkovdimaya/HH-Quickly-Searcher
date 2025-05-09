3ed473d384b4e19c5afc4ca1f265ffd571a68969
volkovdimaya <volkov.dimaya@yandex.ru>
2025-05-09 16:56:38 +0500

    detect магические числа

--- app/src/main/java/ru/practicum/android/diploma/util/FormatterCountVacancyToString.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/FormatterCountVacancyToString.kt
@@ -1,14 +1,25 @@
 package ru.practicum.android.diploma.util
 
 object FormatterCountVacancyToString {
+    const val LAST_DIGIT_MODULO = 10
+    val LAST_TWO_DIGITS_MODULO = 100
+    const val EXCEPTIONAL_LAST_TWO_DIGITS = 11
+    const val ONE_LAST_DIGIT = 1
+    const val EXCEPTIONAL_LAST_TWO_DIGITS_START = 12
+    const val EXCEPTIONAL_LAST_TWO_DIGITS_END = 14
+    const val FEW_LAST_DIGITS_START = 2
+    const val FEW_LAST_DIGITS_END = 4
     operator fun invoke(count: Int): String {
-        val lastDigit = count % 10
-        val lastTwoDigits = count % 100
+        val lastDigit = count % LAST_DIGIT_MODULO
+        val lastTwoDigits = count % LAST_TWO_DIGITS_MODULO
 
         return when {
-            lastDigit == 1 && lastTwoDigits != 11 -> "Найдена $count вакансия"
-            lastDigit in 2..4 && lastTwoDigits !in 12..14 -> "Найдено $count вакансии"
+            lastDigit == ONE_LAST_DIGIT && lastTwoDigits != EXCEPTIONAL_LAST_TWO_DIGITS -> "Найдена $count вакансия"
+            lastDigit in FEW_LAST_DIGITS_START..FEW_LAST_DIGITS_END && lastTwoDigits !in EXCEPTIONAL_LAST_TWO_DIGITS_START..EXCEPTIONAL_LAST_TWO_DIGITS_END -> "Найдено $count вакансии"
             else -> "Найдено $count вакансий"
         }
     }
+
+
+
 }
