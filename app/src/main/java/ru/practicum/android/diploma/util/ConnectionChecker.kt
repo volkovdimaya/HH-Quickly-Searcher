83e2ce87bcbfa9f6f3ca3a8547ac2790ef77f02f
volkovdimaya <volkov.dimaya@yandex.ru>
2025-05-09 15:49:36 +0500

    функция проверки подключения к интернету

---
+++ app/src/main/java/ru/practicum/android/diploma/util/ConnectionChecker.kt
@@ -0,0 +1,21 @@
+package ru.practicum.android.diploma.util
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+fun isConnectedInternet(context: Context): Boolean {
+    val connectivityManager = context.getSystemService(
+        Context.CONNECTIVITY_SERVICE
+    ) as ConnectivityManager
+    val capabilities =
+        connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+    if (capabilities != null) {
+        when {
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> return true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> return true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> return true
+        }
+    }
+    return false
+}
