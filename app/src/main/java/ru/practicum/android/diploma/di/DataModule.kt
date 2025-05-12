bd4a102c5e350aa0d2b6f8c905fabe5bd40e70a0
lyeska <liz-on-chik@mail.ru>
2025-05-13 00:31:31 +0300

    добавлены модули DI и инъекции retrofit и gson

---
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -0,0 +1,19 @@
+package ru.practicum.android.diploma.di
+
+import com.google.gson.Gson
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val dataModule = module {
+
+    single {
+        Retrofit.Builder()
+            .baseUrl("https://api.hh.ru/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    factory { Gson() }
+
+}
