75897f779437554f287003b789611e03b202068a
Aleksandra <rogalevas@yandex.ru>
2025-05-17 17:46:32 +0300

    dp-px formatter fix checkstyle

--- app/src/main/java/ru/practicum/android/diploma/util/SizeFormatter.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/SizeFormatter.kt
@@ -7,7 +7,8 @@ object SizeFormatter {
 
     fun dpToPx(dp: Float, context: Context): Int {
         return TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_DIP, dp,
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp,
             context.resources.displayMetrics
         ).toInt()
     }
