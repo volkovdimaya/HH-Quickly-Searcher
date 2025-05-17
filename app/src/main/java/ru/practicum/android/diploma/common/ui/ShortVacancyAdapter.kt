ebd5588b683b68790aaceaa1b154d2df1f49a0a8
Aleksandra <rogalevas@yandex.ru>
2025-05-17 18:26:06 +0300

    binding FavoritesFragment

---
+++ app/src/main/java/ru/practicum/android/diploma/common/ui/ShortVacancyAdapter.kt
@@ -0,0 +1,3 @@
+package ru.practicum.android.diploma.common.ui
+
+class ShortVacancyAdapter
--- app/src/main/java/ru/practicum/android/diploma/common/ui/ShortVacancyFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/ui/ShortVacancyFragment.kt
@@ -1,27 +1,36 @@
-package ru.practicum.android.diploma.favorites.ui
+package ru.practicum.android.diploma.common.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
 import ru.practicum.android.diploma.R
-import ru.practicum.android.diploma.databinding.FragmentFavoritesBinding
 
-class FavoritesFragment : Fragment() {
+abstract class ShortVacancyFragment<T : ViewBinding> : Fragment() {
 
-    private var _binding: FragmentFavoritesBinding? = null
+    private var _binding: T? = null
     private val binding get() = _binding!!
+
+    private var _recyclerView: RecyclerView? = null
+    private val recyclerView get() = _recyclerView!!
+
+    //   private val progressBar: ProgressBar by lazy { CustomCircularProgressIndicator(requireContext()) }
+    abstract val adapter: ShortVacancyAdapter
 
     private var _includeView: ViewGroup? = null
     private val includeView get() = _includeView!!
+
+    abstract fun createBinding(increateBindingflater: LayoutInflater, container: ViewGroup?): T
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+    ): View {
+        _binding = createBinding(inflater, container)
         _includeView = binding.root.findViewById(R.id.include_view)
         return binding.root
     }
