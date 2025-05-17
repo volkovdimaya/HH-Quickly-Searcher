414fccaade11ba5d0acf99c2786f003279cbdc90
Aleksandra <rogalevas@yandex.ru>
2025-05-17 16:04:43 +0300

    symbol to Currency.kt

--- app/src/main/java/ru/practicum/android/diploma/common/domain/models/Currency.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/domain/models/Currency.kt
@@ -6,15 +6,22 @@ enum class Currency(
     val code: String? = null,
     val default: Boolean = false,
     val currencyName: String,
+    val symbol: String
 ) {
-    RUSSIAN_RUBLE("RUB", abbrVariant = "RUR", default = true, currencyName = "Российский рубль"),
-    BELARUSIAN_RUBLE("BYR", currencyName = "Белорусский рубль"),
-    US_DOLLAR("USD", currencyName = "Доллар"),
-    EURO("EUR", currencyName = "Евро"),
-    KAZAKHSTANI_TENGE("KZT", currencyName = "Казахстанский тенге"),
-    UKRAINIAN_HRYVNIA("UAH", currencyName = "Украинская гривна"),
-    AZERBAIJANI_MANAT("AZN", currencyName = "Азербайджанский манат"),
-    UZBEKISTANI_SOM("UZS", currencyName = "Узбекский сум"),
-    GEORGIAN_LARI("GEL", currencyName = "Грузинский лари"),
-    KYRGYZSTANI_SOM("KGS", currencyName = "Киргизский сом")
+    RUSSIAN_RUBLE(
+        "RUB",
+        abbrVariant = "RUR",
+        default = true,
+        currencyName = "Российский рубль",
+        symbol = '\u20BD'.toString()
+    ),
+    BELARUSIAN_RUBLE("BYR", currencyName = "Белорусский рубль", symbol = "Br"),
+    US_DOLLAR("USD", currencyName = "Доллар", symbol = '\u0024'.toString()),
+    EURO("EUR", currencyName = "Евро", symbol = '\u20AC'.toString()),
+    KAZAKHSTANI_TENGE("KZT", currencyName = "Казахстанский тенге", symbol = '\u20B8'.toString()),
+    UKRAINIAN_HRYVNIA("UAH", currencyName = "Украинская гривна", symbol = '\u20B4'.toString()),
+    AZERBAIJANI_MANAT("AZN", currencyName = "Азербайджанский манат", symbol = '\u20BC'.toString()),
+    UZBEKISTANI_SOM("UZS", currencyName = "Узбекский сум", symbol = "so'm"),
+    GEORGIAN_LARI("GEL", currencyName = "Грузинский лари", symbol = '\u20BE'.toString()),
+    KYRGYZSTANI_SOM("KGS", currencyName = "Киргизский сом", symbol = "сом")
 }
