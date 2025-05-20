b7f728bcbe54b5a0ad11c598170ac671f9e41aca
Aleksandra <rogalevas@yandex.ru>
2025-05-20 15:43:36 +0300

    logic insert/delete favorites

--- app/src/main/java/ru/practicum/android/diploma/common/data/models/VacancyWithWorkTerritory.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/data/models/VacancyWithWorkTerritory.kt
@@ -1,21 +1,16 @@
 package ru.practicum.android.diploma.common.data.models
 
 import androidx.room.Embedded
+import androidx.room.Relation
 import ru.practicum.android.diploma.favorites.data.entity.VacancyEntity
+import ru.practicum.android.diploma.workterritories.data.entity.AreaEntity
 
 data class VacancyWithWorkTerritory(
     @Embedded val vacancy: VacancyEntity,
-//
-//    @Relation(
-//        parentColumn = "workTerritoryRegionId",
-//        entityColumn = "areaId"
-//    )
-//    val region: AreaEntity?,
-//
-//    @Relation(
-//        parentColumn = "workTerritoryCountryId",
-//        entityColumn = "areaId"
-//    )
-//
-//    val country: AreaEntity?
+
+    @Relation(parentColumn = "workTerritoryId", entityColumn = "areaId")
+    val region: AreaEntity?,
+
+    @Relation(parentColumn = "workTerritoryCountryId", entityColumn = "areaId")
+    val country: AreaEntity
 )
