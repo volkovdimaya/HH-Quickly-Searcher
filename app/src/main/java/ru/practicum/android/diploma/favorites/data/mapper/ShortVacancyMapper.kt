4f5963a119bd993d1e059f6bd41c8e561e2dcbec
Aleksandra <rogalevas@yandex.ru>
2025-05-21 21:43:20 +0300

    fix mapping

--- app/src/main/java/ru/practicum/android/diploma/favorites/data/mapper/ShortVacancyMapper.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/data/mapper/ShortVacancyMapper.kt
@@ -1,13 +1,9 @@
 package ru.practicum.android.diploma.favorites.data.mapper
 
-import ru.practicum.android.diploma.common.domain.models.Country
 import ru.practicum.android.diploma.common.domain.models.Currency
-import ru.practicum.android.diploma.common.domain.models.RegionWork
 import ru.practicum.android.diploma.common.domain.models.Salary
 import ru.practicum.android.diploma.common.domain.models.VacancyShort
-import ru.practicum.android.diploma.common.domain.models.WorkTerritory
 import ru.practicum.android.diploma.favorites.data.entity.VacancyEntity
-import ru.practicum.android.diploma.industries.domain.models.Industry
 import ru.practicum.android.diploma.vacancy.domain.models.VacancyDetail
 
 object ShortVacancyMapper {
@@ -25,21 +21,6 @@ object ShortVacancyMapper {
             logoUrl = this.logoUrl,
             employer = this.employer
         )
-    }
-
-    fun mapWorkTerritoryFromString(workTerritoryString: String): WorkTerritory {
-        val country = Country(
-            countryId = 0,
-            countryName = "Россия" // Значение по умолчанию
-        )
-
-        val regionWork = RegionWork(
-            regionId = null,
-            regionName = workTerritoryString,
-            country = country
-        )
-
-        return WorkTerritory(regionWork, country)
     }
 
     fun mapVacancyEntityToVacancyShort(
