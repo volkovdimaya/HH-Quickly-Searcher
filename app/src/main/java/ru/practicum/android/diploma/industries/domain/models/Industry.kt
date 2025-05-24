1545653bd4ce810305eec6bd5cb77ae8edf53ab7
Aleksandra <rogalevas@yandex.ru>
2025-05-24 19:18:34 +0300

    fix data class

--- app/src/main/java/ru/practicum/android/diploma/industries/domain/models/Industry.kt
+++ app/src/main/java/ru/practicum/android/diploma/industries/domain/models/Industry.kt
@@ -3,5 +3,5 @@ package ru.practicum.android.diploma.industries.domain.models
 data class Industry(
     val industryId: String,
     val industryName: String,
-    var select: Boolean = false
+    val select: SelectFlag = SelectFlag.SELECT
 )
---
+++ app/src/main/java/ru/practicum/android/diploma/industries/domain/models/SelectFlag.kt
@@ -0,0 +1,5 @@
+package ru.practicum.android.diploma.industries.domain.models
+
+enum class SelectFlag(var isSelected: Boolean = false) {
+    SELECT
+}
