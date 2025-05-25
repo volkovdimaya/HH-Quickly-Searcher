c5a3bdafc90dde6940aaed8ffb586378b1aaa64d
volkovdimaya <volkov.dimaya@yandex.ru>
2025-05-26 00:18:51 +0500

    фрагмент для показа список стран viewmodel и биндинг подключил, слушаю viewmodel но пока ничего не делаю

--- app/src/main/java/ru/practicum/android/diploma/countries/ui/CountriesFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/countries/ui/CountriesFragment.kt
@@ -5,16 +5,44 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import ru.practicum.android.diploma.R
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.practicum.android.diploma.countries.presentation.CountryViewModel
+import ru.practicum.android.diploma.countries.presentation.models.CountryState
+import ru.practicum.android.diploma.databinding.FragmentCountriesBinding
 
 class CountriesFragment : Fragment() {
+
+    private var _binding: FragmentCountriesBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel by viewModel<CountryViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_countries, container, false)
+        _binding = FragmentCountriesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        val rv = binding.countriesRv
+
+        viewModel.state.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is CountryState.Content -> TODO()
+                CountryState.Loading -> TODO()
+                CountryState.Error -> TODO()
+            }
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
