96e42eb769eff2fe17794007e2e47b877fcc9a38
volkovdimaya <volkov.dimaya@yandex.ru>
2025-05-25 22:06:34 +0500

    entity и dao filter

---
+++ app/src/main/java/ru/practicum/android/diploma/filters/data/entity/FilterParametersEntity.kt
@@ -0,0 +1,29 @@
+package ru.practicum.android.diploma.filters.data.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "filter_parameters")
+data class FilterParametersEntity(
+    @PrimaryKey
+    @ColumnInfo("country_id")
+    val countryId: Int = 0,
+    @ColumnInfo("region_id")
+    val regionId: Int? = null,
+    @ColumnInfo("industry_id")
+    val industryId: Int? = null,
+    @ColumnInfo("salary")
+    val salary: Int? = null,
+    @ColumnInfo("only_with_salary")
+    val onlyWithSalary: Boolean = false,
+    @ColumnInfo("country_name")
+    val countryName: String? = null,
+    @ColumnInfo("region_name")
+    val regionName: String? = null,
+    @ColumnInfo("industry_name")
+    val industryName: String? = null,
+    @ColumnInfo("salary_type")
+    val salaryType: String? = null
+)
