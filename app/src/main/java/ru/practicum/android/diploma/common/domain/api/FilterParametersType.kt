ee27e18fea89a4957672a0bd110886a371e33749
volkovdimaya <volkov.dimaya@yandex.ru>
2025-05-25 18:10:09 +0500

    detect

--- app/src/main/java/ru/practicum/android/diploma/common/domain/api/FilterParametersType.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/domain/api/FilterParametersType.kt
@@ -1,10 +1,10 @@
 package ru.practicum.android.diploma.common.domain.api
 
 sealed class FilterParametersType {
-    class Country(val idCountry: Int) : FilterParametersType()
-    class Region(val idRegion: Int) : FilterParametersType()
-    class Industry(val idIndustry: Int) : FilterParametersType()
-    class Salary(val salary: Int) : FilterParametersType()
+    class Country(val idCountry: Int, val name : String) : FilterParametersType()
+    class Region(val idRegion: Int, val name : String) : FilterParametersType()
+    class Industry(val idIndustry: Int, val name : String) : FilterParametersType()
+    class Salary(val salary: Int, val type : String) : FilterParametersType()
     class OnlyWithSalary(val onlyWithSalary: Boolean) : FilterParametersType()
 
 }
