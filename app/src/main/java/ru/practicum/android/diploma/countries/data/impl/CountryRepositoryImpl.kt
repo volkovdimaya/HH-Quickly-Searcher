16784980ff13ef0b92b8cca3b157626f6eccbf65
volkovdimaya <volkov.dimaya@yandex.ru>
2025-05-25 23:50:46 +0500

    имя класса отражает где он находится

--- app/src/main/java/ru/practicum/android/diploma/countries/data/impl/CountryRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/countries/data/impl/CountryRepositoryImpl.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import ru.practicum.android.diploma.common.domain.models.Country
 import ru.practicum.android.diploma.countries.domain.api.CountryRepository
-import ru.practicum.android.diploma.countries.domain.models.CountryResponse
+import ru.practicum.android.diploma.countries.domain.models.CountryResponseDomain
 import ru.practicum.android.diploma.regions.data.dto.AreasRequest
 import ru.practicum.android.diploma.regions.data.dto.AreasResponse
 import ru.practicum.android.diploma.search.data.network.NetworkClient
@@ -15,17 +15,17 @@ class CountryRepositoryImpl(
     private val networkClient: NetworkClient,
     private val application: Application
 ) : CountryRepository {
-    override suspend fun getCountries(): Flow<CountryResponse> = flow {
+    override suspend fun getCountries(): Flow<CountryResponseDomain> = flow {
         if (isConnectedInternet(application)) {
             emit(getCountriesFromNetwork())
         } else {
-            emit(CountryResponse(BAD_REQUEST_CODE))
+            emit(CountryResponseDomain(BAD_REQUEST_CODE))
         }
     }
 
-    private suspend fun getCountriesFromNetwork(): CountryResponse {
+    private suspend fun getCountriesFromNetwork(): CountryResponseDomain {
         val networkResponse = networkClient.doRequest(AreasRequest())
-        val response = CountryResponse(networkResponse.resultCode)
+        val response = CountryResponseDomain(networkResponse.resultCode)
         if (networkResponse is AreasResponse) {
             response.countries = networkResponse.areas.filter { area ->
                 area.parentId == null
--- app/src/main/java/ru/practicum/android/diploma/countries/domain/api/CountryRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/countries/domain/api/CountryRepository.kt
@@ -1,8 +1,8 @@
 package ru.practicum.android.diploma.countries.domain.api
 
 import kotlinx.coroutines.flow.Flow
-import ru.practicum.android.diploma.countries.domain.models.CountryResponse
+import ru.practicum.android.diploma.countries.domain.models.CountryResponseDomain
 
 interface CountryRepository {
-    suspend fun getCountries(): Flow<CountryResponse>
+    suspend fun getCountries(): Flow<CountryResponseDomain>
 }
