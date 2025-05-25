220f5467d79c0938740a4223e72d551af9c79883
volkovdimaya <volkov.dimaya@yandex.ru>
2025-05-25 22:55:35 +0500

    добавил необходимые зависимости к AppDatabase, изменил версию db не забудь сделать миграцию!!!

--- app/src/main/java/ru/practicum/android/diploma/common/data/db/AppDatabase.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/data/db/AppDatabase.kt
@@ -6,18 +6,26 @@ import androidx.room.TypeConverters
 import ru.practicum.android.diploma.common.data.dao.AreaDao
 import ru.practicum.android.diploma.common.data.dao.VacancyDao
 import ru.practicum.android.diploma.favorites.data.entity.VacancyEntity
+import ru.practicum.android.diploma.filters.data.dao.FilterClearParametersDao
+import ru.practicum.android.diploma.filters.data.dao.FilterParametersCreateDao
+import ru.practicum.android.diploma.filters.data.dao.FilterUpdateParametersDao
+import ru.practicum.android.diploma.filters.data.entity.FilterParametersEntity
 import ru.practicum.android.diploma.vacancy.mapper.StringListConverter
 import ru.practicum.android.diploma.workterritories.data.entity.AreaEntity
 
 @Database(
-    version = 5,
+    version = 6,
     entities = [
         VacancyEntity::class,
         AreaEntity::class,
+        FilterParametersEntity::class
     ]
 )
 @TypeConverters(StringListConverter::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun vacancyDao(): VacancyDao
     abstract fun areaDao(): AreaDao
+    abstract fun filterParametersCreateDao(): FilterParametersCreateDao
+    abstract fun filterUpdateParametersDao(): FilterUpdateParametersDao
+    abstract fun filterClearParametersDao(): FilterClearParametersDao
 }
