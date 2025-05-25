0ccfa1869ece959ca5991d557adc81ca84d2c242
Aleksandra <rogalevas@yandex.ru>
2025-05-25 21:17:45 +0300

    check

--- app/src/main/java/ru/practicum/android/diploma/common/data/db/AppDatabase.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/data/db/AppDatabase.kt
@@ -4,20 +4,23 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import ru.practicum.android.diploma.common.data.dao.AreaDao
+import ru.practicum.android.diploma.common.data.dao.IndustryDao
 import ru.practicum.android.diploma.common.data.dao.VacancyDao
 import ru.practicum.android.diploma.favorites.data.entity.VacancyEntity
 import ru.practicum.android.diploma.filters.data.dao.FilterClearParametersDao
 import ru.practicum.android.diploma.filters.data.dao.FilterParametersCreateDao
 import ru.practicum.android.diploma.filters.data.dao.FilterUpdateParametersDao
 import ru.practicum.android.diploma.filters.data.entity.FilterParametersEntity
+import ru.practicum.android.diploma.industries.data.entity.IndustryEntity
 import ru.practicum.android.diploma.vacancy.mapper.StringListConverter
 import ru.practicum.android.diploma.workterritories.data.entity.AreaEntity
 
 @Database(
-    version = 6,
+    version = 7,
     entities = [
         VacancyEntity::class,
         AreaEntity::class,
+        IndustryEntity::class,
         FilterParametersEntity::class
     ]
 )
@@ -25,6 +28,7 @@ import ru.practicum.android.diploma.workterritories.data.entity.AreaEntity
 abstract class AppDatabase : RoomDatabase() {
     abstract fun vacancyDao(): VacancyDao
     abstract fun areaDao(): AreaDao
+    abstract fun industryDao(): IndustryDao
     abstract fun filterParametersCreateDao(): FilterParametersCreateDao
     abstract fun filterUpdateParametersDao(): FilterUpdateParametersDao
     abstract fun filterClearParametersDao(): FilterClearParametersDao
