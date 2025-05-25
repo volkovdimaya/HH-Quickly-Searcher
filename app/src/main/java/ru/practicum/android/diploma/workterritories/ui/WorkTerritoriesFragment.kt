6cd92afa45a2477afca1bdca20bbbd7e453d58ba
volkovdimaya <volkov.dimaya@yandex.ru>
2025-05-26 02:07:46 +0500

    переходы из WorkTerritoriesFragment в страны и регионы

--- app/src/main/java/ru/practicum/android/diploma/workterritories/ui/WorkTerritoriesFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/workterritories/ui/WorkTerritoriesFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentWorkTerritoriesBinding
@@ -25,6 +26,17 @@ class WorkTerritoriesFragment : Fragment() {
     ): View? {
         _binding = FragmentWorkTerritoriesBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.country.setOnClickListener {
+            findNavController().navigate(R.id.countriesFragment)
+        }
+        binding.region.setOnClickListener {
+            findNavController().navigate(R.id.regionsFragment)
+        }
     }
 
 
