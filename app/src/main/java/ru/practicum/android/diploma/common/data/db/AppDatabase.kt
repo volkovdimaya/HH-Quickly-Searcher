b55bdb0301658fbc2af8cfa97063655c5129f4a7
volkovdimaya <volkov.dimaya@yandex.ru>
2025-05-25 20:55:35 +0300

    добавил необходимые зависимости к AppDatabase, изменил версию db не забудь сделать миграцию!!!

--- app/src/main/java/ru/practicum/android/diploma/common/data/db/AppDatabase.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/data/db/AppDatabase.kt
@@ -4,10 +4,12 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import ru.practicum.android.diploma.common.data.dao.AreaDao
-import ru.practicum.android.diploma.common.data.dao.IndustryDao
 import ru.practicum.android.diploma.common.data.dao.VacancyDao
 import ru.practicum.android.diploma.favorites.data.entity.VacancyEntity
-import ru.practicum.android.diploma.industries.data.entity.IndustryEntity
+import ru.practicum.android.diploma.filters.data.dao.FilterClearParametersDao
+import ru.practicum.android.diploma.filters.data.dao.FilterParametersCreateDao
+import ru.practicum.android.diploma.filters.data.dao.FilterUpdateParametersDao
+import ru.practicum.android.diploma.filters.data.entity.FilterParametersEntity
 import ru.practicum.android.diploma.vacancy.mapper.StringListConverter
 import ru.practicum.android.diploma.workterritories.data.entity.AreaEntity
 
@@ -16,12 +18,14 @@ import ru.practicum.android.diploma.workterritories.data.entity.AreaEntity
     entities = [
         VacancyEntity::class,
         AreaEntity::class,
-        IndustryEntity::class
+        FilterParametersEntity::class
     ]
 )
 @TypeConverters(StringListConverter::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun vacancyDao(): VacancyDao
     abstract fun areaDao(): AreaDao
-    abstract fun industryDao(): IndustryDao
+    abstract fun filterParametersCreateDao(): FilterParametersCreateDao
+    abstract fun filterUpdateParametersDao(): FilterUpdateParametersDao
+    abstract fun filterClearParametersDao(): FilterClearParametersDao
 }
