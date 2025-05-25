0fa577d6c8507527d644f8f1d09f542d3492fa76
volkovdimaya <volkov.dimaya@yandex.ru>
2025-05-25 22:24:05 +0500

    detect разделил на несколько интерфейсов где обновляем фильтры создаем если нет и очищаем

--- app/src/main/java/ru/practicum/android/diploma/filters/data/entity/FilterParametersEntity.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/data/entity/FilterParametersEntity.kt
@@ -3,7 +3,6 @@ package ru.practicum.android.diploma.filters.data.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "filter_parameters")
 data class FilterParametersEntity(
