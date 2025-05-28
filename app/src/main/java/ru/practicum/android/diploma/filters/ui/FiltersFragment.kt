2ba3c62180ba72f1d19787a1e9017a354cf0efd0
Aleksandra <rogalevas@yandex.ru>
2025-05-28 09:41:51 +0300

    conf

--- app/src/main/java/ru/practicum/android/diploma/filters/ui/FiltersFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/ui/FiltersFragment.kt
@@ -105,8 +105,8 @@ class FiltersFragment : Fragment() {
         binding.workTerritoriesSelected.isVisible = filters.countryId != null
         binding.workTerritoryName.text = makeWorkTerritoryName(filters)
 
-        binding.industry.isVisible = filters.industryId == null
-        binding.industrySelected.isVisible = filters.industryId != null
+        binding.industry.isVisible = filters.industryId.isNullOrBlank()
+        binding.industrySelected.isVisible = !filters.industryId.isNullOrBlank()
         binding.industryName.text = filters.industryName ?: ""
 
         binding.expectedSalaryEditText.setText(filters.salary?.toString() ?: "")
