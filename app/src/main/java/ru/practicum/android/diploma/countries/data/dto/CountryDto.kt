df970b3cdb072fb4414b8c6451e2524179173818
Aleksandra <rogalevas@yandex.ru>
2025-06-03 02:43:23 +0300

    лишний параметр в дто

--- app/src/main/java/ru/practicum/android/diploma/countries/data/dto/CountryDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/countries/data/dto/CountryDto.kt
@@ -1,10 +1,6 @@
 package ru.practicum.android.diploma.countries.data.dto
 
-import com.google.gson.annotations.SerializedName
-
 data class CountryDto(
     val id: Int,
-    val name: String,
-    @SerializedName("parent_id")
-    val parentId: String? = null,
+    val name: String
 )
